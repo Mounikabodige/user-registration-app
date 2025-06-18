76ba246a6ba0ac8db633e5c4623e949d37e46945
mounikabodige <mounikabodige1994@gmail.com>
2025-06-18 17:03:23 -0600

    Encapsulation

--- src/app/form-status/form-status.component.ts
+++ src/app/form-status/form-status.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-form-status',
   templateUrl: './form-status.component.html',
-  styleUrls: ['./form-status.component.css']
+  styleUrls: ['./form-status.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FormStatusComponent implements OnInit {
+
+  @Input() message = '';
+  @Input() isError = false;
 
   constructor() { }
 
