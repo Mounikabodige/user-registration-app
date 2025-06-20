61f8666b8cee3e85dcc5a0f1a1540f070b27344d
mounikabodige <mounikabodige1994@gmail.com>
2025-06-20 13:27:34 -0600

    Migration angualr 15 -> 16

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   isLoggedIn = false; 
 
   constructor(private auth : AuthService,private router: Router) {}
